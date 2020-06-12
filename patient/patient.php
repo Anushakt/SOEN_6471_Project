4ad961df8c1c6b800105c1711e6fbafd610f0a57
AnushaKt <kt.anusha21@gmail.com>
2020-06-12 15:52:41 -0400

    Adding Patient View page

--- patient/patient.php
+++ patient/patient.php
@@ -45,3 +45,116 @@ $success="<div class=\"alert alert-success\" role=\"alert\">
 }
 }
 ?>
+<div class="container">
+  <h1 class="display-1 text-justify text-danger">iCare </h1>
+  <div class="row">
+    <div class="col-md-10">
+  <h1 class="display-4 text-left">Sign Up </h1>
+</div>
+<div class="col-md-2">
+   <form action="../index.php">
+ <button type="submit" class="btn btn-outline-success btn-lg">Sign in</button>
+ </form>
+  </div>
+</div>
+  <?php echo $success; echo $fail; ?>
+<form action="" method="post">
+	<div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Last Name</label>
+      <input type="text" class="form-control" id="inputEmail4" required="required" name="lname">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Other Name(s)</label>
+      <input type="text" class="form-control" id="inputPassword4" required="required" name="oname">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" required="required" name="email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" id="inputPassword4" required="required" name="password">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-2">
+  <div class="form-check">
+  <input class="form-check-input" type="radio" name="sex" id="exampleRadios1" value="male" checked=checked>
+  <label class="form-check-label" for="exampleRadios1">
+    Male
+  </label>
+</div>
+ </div>
+  <div class="form-group col-md-2">
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="sex" id="exampleRadios2" value="female">
+  <label class="form-check-label" for="exampleRadios2">
+    Female
+  </label>
+</div>
+</div>
+<div class="form-group col-md-8"></div>
+</div>
+<div class="form-row">
+   <div class="form-group col-md-2">
+      <label for="inputEmail4">Date of Birth: </label>
+    </div>
+    <div class="form-group col-md-3">
+      <label for="inputEmail4">Day</label>
+      <input type="number" class="form-control" id="inputEmail4" required="required" name="day">
+    </div>
+    <div class="form-group col-md-3">
+      <label for="inputPassword4">Month</label>
+      <input type="number" class="form-control" id="inputPassword4" required="required" name="month">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputPassword4">Year</label>
+      <input type="number" class="form-control" id="inputPassword4" required="required" name="year">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Height (cm)</label>
+      <input type="text" class="form-control" id="inputEmail4" required="required" name="height">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Weight (lb)</label>
+      <input type="text" class="form-control" id="inputPassword4" required="required" name="weight">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" required="required" name="address">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity" required="required" name="city">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control" required="required" name="state">
+        <option selected>Choose...</option>
+        <option value="quebec">Quebec</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip" required="required" name="zip">
+    </div>
+  </div>
+  <button type="submit" name="save" class="btn btn-primary">Sign Up</button>
+</form>
+
+</div>
+
+</body>
+</html>
+
+<?php
+
+
+?>
