d8970df490c9c3ff1f8399cceb87f01f1b915fcc
Niral H Lad <43843579+niralhlad@users.noreply.github.com>
2020-06-12 11:15:29 -0400

    Create doctor_home.php
    Doctore home completed

---
+++ doctor_home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>iCare</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container">
+  <h1 class="display-1 text-justify text-danger">iCare </h1>
+  <h1 class="display-5 text-left">Doctor Dashboard </h1>
+ </div>
+
+  <div class="container jumbotron jumbotron-fluid"> 	
+     <div class="row col">
+    <p class="text-success">Welcome! display Doctor name </p>
+ </div>
+ <p></p>
+ <div class="row col">
+<nav class="nav flex-column">
+  <a class="nav-link active" href="#">Booking Confirmation</a>
+  <a class="nav-link" href="#">My Profile</a>
+</nav>
+</div>
+</div>
+</body>
+</html>
