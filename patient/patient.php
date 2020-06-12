201529c8ea99d1bc457cae4972661ebc64398a6c
AnushaKt <kt.anusha21@gmail.com>
2020-06-12 15:52:06 -0400

    Adding Patient php file

--- patient/patient.php
+++ patient/patient.php
@@ -10,10 +10,38 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<?php  
+include("../connection.php");
+$success=$fail="";
+if(isset($_POST['save'])){
+  $lname = $_POST['lname'];
+  $oname = $_POST['oname'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $sex = $_POST['sex'];
+  $day = $_POST['day'];
+  $month = $_POST['month'];
+  $year = $_POST['year'];
+  $height = $_POST['height'];
+  $weight = $_POST['weight'];
+  $address = $_POST['address'];
+  $city = $_POST['city'];
+  $state = $_POST['state'];
+  $zip = $_POST['zip'];
 
-</html>
 
-<?php
+  $em = mysqli_query($connection, "SELECT * FROM patient_tbl WHERE email = '$email'")or die(mysqli_error($connection));
+  if(mysqli_num_rows($em)==1){
+    $fail = "<div class=\"alert alert-danger\" role=\"alert\">
+  The email you entered already exist!
+</div>";
+}else{
 
-
+  mysqli_query($connection, "INSERT INTO patient_tbl(lname,oname,email,password,sex,day,month,year,height,weight,address,city,state,zip)VALUES('$lname',
+    '$oname','$email','$password','$sex','$day','$month','$year','$height','$weight','$address','$city','$state','$zip')")or die(mysqli_error($connection));
+$success="<div class=\"alert alert-success\" role=\"alert\">
+  You have registered successfully!
+</div>";
+}
+}
 ?>
